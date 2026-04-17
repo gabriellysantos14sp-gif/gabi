6afbb860cd2917c60660fc1dec5c8fda73205411
gabriellysantos14sp-gif <gabriellysantos14sp@gmail.com>
2026-04-17 14:45:21 -0300

    finalizando codigo

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+function mudar_estilo(){
+
+document.getElementById("paragrafo").style.textAlign = "right";
+}
+function mudar_estilo(){
+
+document.getElementById("paragrafo").style.textAlign = "right";
+}
 
